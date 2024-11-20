6b2742d93a919bff7ad04812eadca5684c1e893c
James Kalu <jamesiukalu@gmail.com>
2024-11-20 12:37:48 -0500

    ok

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://techtalize.netlify.app/'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://techtalize.netlify.app/', '18.234.215.162'],
   credentials: true,
 }));
 
